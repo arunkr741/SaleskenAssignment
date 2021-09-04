fbf9c3767b31dd6cc18b91d98c9cb35cdee82bd6
unknown <saxena1046@hotmail.com>
2021-09-04 17:01:58 +0530

    demo song added

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var c = document.getElementById("myCanvas");
 var ctx = c.getContext("2d");
 var playbtn = document.getElementById("playbtn")
+let curr_track = document.getElementById('audio');
+
 var startX 
 var startY
 
@@ -10,10 +12,10 @@ ctx.lineWidth = "0";
 ctx.strokeStyle = "red";
 ctx.fillStyle = "lightgrey"
 
-let y=[120,130, 100,123, 150, 140,159,160,130,110,120,147,134,135,150,139,132,139]
-let height = [110,120,147,134,135,150,139,132,139,120,130, 100,123, 150, 140,159,160,130]
+let y=[120,120, 120,100, 150, 70,159,160,130,110,120,147,134,135,150,139,132,139,110,130,150,140,135,150,139,132,139,120,130, 100,123, 150, 140,159,160,130,110,130,150,140,135,150,139,132,139,120,130, 100,123, 150, 140,159,160,130,120,120, 120,100, 150, 70,159,160,130,110,120,147,134,135,150,139,132,139,150, 140,159,160,130,110,130,150,140,135,150,139]
+let height = [110,130,150,140,135,150,139,132,139,120,130, 100,123, 150, 140,159,160,130,120,120, 120,100, 150, 70,159,160,130,110,120,147,134,135,150,139,132,139,120,120, 120,100, 150, 70,159,160,130,110,120,147,134,135,150,139,132,139,110,130,150,140,135,150,139,132,139,120,130, 100,123, 150, 140,159,160,130,139,132,139,120,130, 100,123, 150, 140,159,160,130]
 var j = 0
-for(var i=0;i<y.length;i++){
+for(let i=0;i<y.length;i++){
     
      ctx.fillRect(j, y[i], 5, height[i]);
      j+=10
@@ -22,28 +24,38 @@ let flag = true
 
  var interval
             j=0
-            
-            i=0
+           var index=0
+           var isPlaying = false;      
+
+
     function start(i,j,clickedincanvas) {
         console.log(i,j)
-        if(flag ){
+    
+       playpauseTrack()
+        if(flag || clickedincanvas ){
+            playbtn.innerHTML=`<i class="fa fa-pause"></i>`
+            clearInterval(interval)
              interval = setInterval(()=>{
-                ctx.fillStyle = "black"
+                ctx.fillStyle = "rgb(221, 127, 149)"
                 ctx.fillRect(j, y[i], 5, height[i]);
                 j+=10
                 i++
+               
             },1000)
             flag=false
         }
         else{
             clearInterval(interval)
             flag=true
+            playbtn.innerHTML=`<i class="fa fa-play"></i>`
         }  
     }
-    playbtn.addEventListener("click",()=>{
-        start(i,j)
-    })
+    
 
+    playbtn.addEventListener("click",()=>{
+        console.log(index)
+        start(index,j)
+    })
 
 
     function filled(e) {
@@ -59,22 +71,43 @@ let flag = true
         }
         j=0
         for(var i=0;i<startX/10;i++){
-            ctx.fillStyle = "black"
+            ctx.fillStyle = "rgb(221, 127, 149)"
                 ctx.fillRect(j, y[i], 5, height[i]);
                 j+=10
         }
         console.log(i,j)
+        index=i
         start(i,j,true)
         // console.log(startX,startY) 
     }
 
     c.addEventListener("mousedown",(e)=>{
         filled(e)
-        console.log(1)
+       
     })
-
-
-
 
 ctx.stroke();
 
+function loadTrack(){
+    curr_track.src="https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Shipping_Lanes.mp3"
+    curr_track.load()
+}
+loadTrack()
+function playpauseTrack() {
+    console.log("playpauseTrack")
+    if (!isPlaying) playTrack();
+    else pauseTrack();
+  }
+
+  function playTrack() {
+    console.log("playTrack")
+    curr_track.play();
+    isPlaying = true;
+    console.log(isPlaying)
+  }
+
+  function pauseTrack() {
+    console.log("pauseTrack")
+    curr_track.pause();
+    isPlaying = false;
+  }
